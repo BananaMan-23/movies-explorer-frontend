daa5498b41e9715b785be6a5ae90c23d5a02c5ce
BananaMan <suvorov.andrey755@gmail.com>
2023-10-20 00:43:28 +0300

    надо переделать

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const HEADERS = {
 
 export const BASE_URL_MOVIES_API = 'https://api.nomoreparties.co';
 
-export const BASE_URL = 'http://api.diplom.films.nomoredomainsrocks.ru';
+export const BASE_URL = 'http:/localhost:3000';
 
 export const AMOUNT_CARDS_FOR_SMALL_SCREEN = 5;
 export const AMOUNT_CARDS_FOR_LARGE_SCREEN = 4;
